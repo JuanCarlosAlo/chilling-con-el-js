8744269f23866a8074d23e97715df1bce772b9ee
JC <jc.alonsome@gmail.com>
2023-01-27 23:12:31 +0100

    ok

--- relax the trilogy/src/js/index.js
+++ relax the trilogy/src/js/index.js
@@ -1,15 +1,15 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
-import { printName, printNumber, printThing } from '../js/printText.js';
+import "../scss/styles.scss";
+import { printName, printNumber, printThing } from "../js/printText.js";
 
-const buttons = document.getElementById('buttons');
+const buttons = document.getElementById("buttons");
 
-buttons.addEventListener('click', e => {
-  if (e.target.dataset.item === 'name') {
+buttons.addEventListener("click", (e) => {
+  if (e.target.dataset.item === "name") {
     printName(e.target.dataset.item);
-  } else if (e.target.dataset.item === 'number') {
+  } else if (e.target.dataset.item === "number") {
     printNumber(e.target.dataset.item);
-  } else if (e.target.dataset.item === 'thing') {
+  } else if (e.target.dataset.item === "thing") {
     printThing(e.target.dataset.item);
   }
 });
--- relax the trilogy/src/js/printText.js
+++ relax the trilogy/src/js/printText.js
@@ -1,32 +1,30 @@
-import { DESTINY_ITEMS } from './const.js';
+import { DESTINY_ITEMS } from "./const.js";
 
-const destinyTitle = document.getElementById('destiny-title');
+const destinyTitle = document.getElementById("destiny-title");
 
-let name;
-let number;
-let thing;
+let name = "";
+let number = "";
+let thing = "";
 
 const writePhrase = () => {
-  destinyTitle.textContent = `${name} se merece ${number} ${thing}`;
+  destinyTitle.textContent = `${name} ${number} ${thing}`;
 };
 
-const randomNumber = arrayLength => Math.floor(Math.random() * arrayLength);
+const randomNumber = (arrayLength) => Math.floor(Math.random() * arrayLength);
 
 const printName = () => {
-  const arrayLength = DESTINY_ITEMS.names.length;
-  randomNumber(arrayLength);
-  name = randomNumber(arrayLength);
+  const nameLength = DESTINY_ITEMS.names.length;
+  name = DESTINY_ITEMS.names[randomNumber(nameLength)];
   writePhrase();
 };
 const printNumber = () => {
-  const arrayLength = DESTINY_ITEMS.numbers.length;
-  number = randomNumber(arrayLength);
+  const nameLength = DESTINY_ITEMS.numbers.length;
+  number = "se merece " + DESTINY_ITEMS.numbers[randomNumber(nameLength)];
   writePhrase();
 };
 const printThing = () => {
-  const arrayLength = DESTINY_ITEMS.things.length;
-  randomNumber();
-  thing = randomNumber(arrayLength);
+  const nameLength = DESTINY_ITEMS.things.length;
+  thing = DESTINY_ITEMS.things[randomNumber(nameLength)];
   writePhrase();
 };
 
